ffe71dac7fa362e7fce369466de5a693071d60b0
Ali Elahi <alielahi.1378@gmail.com>
2022-03-03 17:53:51 +0330

    Creating catalog templates

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, Category
+
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'price', 'available', 'created',
+                    'updated')
+    list_display_links = ('id', 'name')
+    list_filter = ('available', 'created')
+    list_editable = ('price', 'available')
+    prepopulated_fields = {'slug': ('name',), }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
