802c7e5cf3b5ab58c99bd39c86e8f101780f8fcd
AliE99 <alielahi.1378@gmail.com>
2022-03-30 16:34:21 +0430

    Django rest framework is installed

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'playground',
     'debug_toolbar',
     'store',
