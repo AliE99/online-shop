198dd431bee0f64b7f9c8a5d3b4be6331f3923c1
AliE99 <alielahi.1378@gmail.com>
2022-06-06 15:49:35 +0430

    Feat: Building an API to upload images

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -34,6 +34,7 @@ from .models import (
     Order,
     OrderItem,
     Product,
+    ProductImage,
     Review,
 )
 from .serializers import (
@@ -44,6 +45,7 @@ from .serializers import (
     CreateOrderSerializer,
     CustomerSerializer,
     OrderSerializer,
+    ProductImageSerializer,
     ProductSerializer,
     ReviewSerializer,
     UpdateCartItemSerializer,
@@ -189,3 +191,15 @@ class OrderViewSet(ModelViewSet):
 
         customer_id = Customer.objects.only("id").get(user_id=user.id)
         return Order.objects.filter(customer_id=customer_id)
+
+
+class ProductImageViewSet(ModelViewSet):
+    serializer_class = ProductImageSerializer
+
+    def get_queryset(self):
+        return ProductImage.objects.filter(
+            product_id=self.kwargs["product_pk"]
+        )
+
+    def get_serializer_context(self):
+        return {"product_id": self.kwargs["product_pk"]}
